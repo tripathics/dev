c5edbe36b06d956453e182f0cb65d5164c7e954a
Chandrashekhar Tripathi <tripathics@outlook.com>
2026-08-24 00:00:20 +0530

    refactor(nvim): move nvim config out of dotfiles and track as git submodule
    Extract the entire neovim configuration into a dedicated repository
    (tripathics/dotnvim) and reference it as a submodule at
    dotfiles/.config/nvim via .gitmodules.
    
    All previously in-repo nvim files (init.lua, lua/*, plugin specs, lsp/
    configs, ftplugin rules, lockfile, stylua.toml) are removed, as they are
    now maintained upstream at df02265.

--- dotfiles/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require 'settings'
-require 'keymaps'
-require 'autocmds'
-require 'lsp'
-
-require('vim._core.ui2').enable {}
--- dotfiles/.config/nvim/plugin/fidget-nvim.lua
+++
@@ -1,9 +0,0 @@
-local add = require('utils.pack').add
-
-add {
-    {
-        src = 'j-hui/fidget.nvim',
-        name = 'fidget',
-        config = function() require('fidget').setup {} end,
-    },
-}
